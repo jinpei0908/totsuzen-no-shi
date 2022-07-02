738a09f273b1e4209b2e45c95ee1600f49377f09
jinpei0908 <jinpei0908@gmail.com>
2022-07-03 01:00:27 +0900

    format/lint対応

--- denops/totsuzen/test.ts
+++ denops/totsuzen/test.ts
@@ -1,13 +1,17 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { decorate } from "./main.ts";
-import stringWidth from "https://esm.sh/string-width";
-import { round } from "https://deno.land/x/math@v1.1.0/mod.ts";
 
 Deno.test("decorate", () => {
   const testCases = [
     { str: "突然の死", expected: ["＿人人人人人人＿", "＞　突然の死　＜", "￣Y^Y^Y^Y^Y^Y^Y￣"] },
-    { str: "hello 世界", expected: ["＿人人人人人人人＿", "＞　hello 世界　＜", "￣Y^Y^Y^Y^Y^Y^Y^Y￣"] },
-    { str: "👨‍👩‍👧‍👦 😄", expected: ["＿人人人人人＿", "＞　👨‍👩‍👧‍👦 😄　＜", "￣Y^Y^Y^Y^Y^Y￣"] },
+    {
+      str: "hello 世界",
+      expected: ["＿人人人人人人人＿", "＞　hello 世界　＜", "￣Y^Y^Y^Y^Y^Y^Y^Y￣"],
+    },
+    {
+      str: "👨‍👩‍👧‍👦 😄",
+      expected: ["＿人人人人人＿", "＞　👨‍👩‍👧‍👦 😄　＜", "￣Y^Y^Y^Y^Y^Y￣"],
+    },
   ];
 
   testCases.forEach((testCase) => {
