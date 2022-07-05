fa6d95d062ec882e9577ad19d66e234fd7137919
jinpei0908 <jinpei0908@gmail.com>
2022-07-05 22:17:14 +0900

    複数行出力

--- denops/totsuzen/main.ts
+++ denops/totsuzen/main.ts
@@ -3,16 +3,45 @@ import { ensureString } from "https://deno.land/x/unknownutil@v0.1.1/mod.ts";
 import stringWidth from "https://esm.sh/string-width";
 import { round } from "https://deno.land/x/math@v1.1.0/mod.ts";
 
+const PADDING_SIZE = 2;
+
+/**
+ * 最大表示幅を求める
+ */
+export function getMaxDispalyWidth(texts: string[]): number {
+  return texts
+    .map((t) => stringWidth(t))
+    .reduce((a, b) => a > b ? a : b);
+}
+
+/**
+ * 真ん中の文字列を作成する
+ */
+export function createMedium(
+  text: string,
+  maxDisplayWidth: number,
+): string {
+  const length = [...text].length;
+  const padding = length + (maxDisplayWidth - stringWidth(text));
+  return `＞　${text.padEnd(padding)}　＜`;
+}
+
 /**
  * textを装飾した文字列のリストを返す
  */
 export function decorate(text: string): string[] {
-  const doubledDiplayWidth = stringWidth(text);
-  const displayWidth = Number(round(doubledDiplayWidth / 2, 0));
-  const top = `＿${"人".repeat(displayWidth + 2)}＿`;
-  const medium = `＞　${text}　＜`;
-  const bottom = `￣${"Y^".repeat(displayWidth + 2)}Y￣`;
-  return [top, medium, bottom];
+  // 改行でtextを分割する
+  const texts = text.split(/\\r\\n|\\r|\\n/);
+
+  const maxDisplayWidth = getMaxDispalyWidth(texts);
+  const repeatCount = Number(round(maxDisplayWidth / 2, 0));
+
+  const top = `＿${"人".repeat(repeatCount + PADDING_SIZE)}＿`;
+  const medium = texts
+    .map((t) => createMedium(t, maxDisplayWidth));
+  const bottom = `￣${"Y^".repeat(repeatCount + PADDING_SIZE)}Y￣`;
+
+  return [top, ...medium, bottom];
 }
 
 export async function main(denops: Denops): Promise<void> {
